1ad6fe84d678ce5264530e0c265ce7235a62c172
IgorKlopov <igor@klopov.com>
2014-03-15 22:47:05 -0700

    Removed spacing

--- test/main.js
+++ test/main.js
@@ -8,22 +8,20 @@ require('mocha');
 
 describe('gulp-concat', function() {
   describe('concat()', function() {
-
-    testFiles(concat('test.js'                   ), ['wadup'], 'wadup');
+    testFiles(concat('test.js'), ['wadup'], 'wadup');
     testFiles(concat('test.js', {newLine: '\r\n'}), ['wadup'], 'wadup');
-    testFiles(concat('test.js', {newLine: ''    }), ['wadup'], 'wadup');
+    testFiles(concat('test.js', {newLine: ''}), ['wadup'], 'wadup');
 
-    testFiles(concat('test.js'                   ), ['wadup', 'doe'], 'wadup\ndoe');
+    testFiles(concat('test.js'), ['wadup', 'doe'], 'wadup\ndoe');
     testFiles(concat('test.js', {newLine: '\r\n'}), ['wadup', 'doe'], 'wadup\r\ndoe');
-    testFiles(concat('test.js', {newLine: ''    }), ['wadup', 'doe'], 'wadupdoe');
+    testFiles(concat('test.js', {newLine: ''}), ['wadup', 'doe'], 'wadupdoe');
 
-    testFiles(concat('test.js'                   ), ['wadup', 'doe', 'hey'], 'wadup\ndoe\nhey');
+    testFiles(concat('test.js'), ['wadup', 'doe', 'hey'], 'wadup\ndoe\nhey');
     testFiles(concat('test.js', {newLine: '\r\n'}), ['wadup', 'doe', 'hey'], 'wadup\r\ndoe\r\nhey');
-    testFiles(concat('test.js', {newLine: ''    }), ['wadup', 'doe', 'hey'], 'wadupdoehey');
+    testFiles(concat('test.js', {newLine: ''}), ['wadup', 'doe', 'hey'], 'wadupdoehey');
 
     function testFiles(stream, contentses, result) {
       it('should concat one or several files', function(done) {
-
         stream.on('data', function(newFile){
           should.exist(newFile);
           should.exist(newFile.path);
@@ -50,9 +48,7 @@ describe('gulp-concat', function() {
         });
 
         stream.end();
-
       });
     };
-
   });
 });
